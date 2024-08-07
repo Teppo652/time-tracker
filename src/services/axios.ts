529d891be3fa04e5d207fb4160647e9036e2967b
Teppo652 <57330214+Teppo652@users.noreply.github.com>
2024-08-07 11:26:18 +0200

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-//baseURL: process.env.VUE_APP_API_URL || "http://localhost:5173",
-// baseURL: "http://localhost:5173",
-//const API_URL = process.env.VUE_APP_API_URL + "/employees";
-
 const port = "3000";
 const baseURL = `http://localhost:${port}`;
 
